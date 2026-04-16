d2ca0a662227e7f3bc608cdbdc05b0b769321759
DeepakKolli <154232629+DeepakKolli@users.noreply.github.com>
2026-04-17 00:10:03 +0530

    models of module 5
    Booking js
    Pool js

--- models/Booking.js
+++ models/Booking.js
@@ -4,16 +4,25 @@ const bookingSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true
+        required: true,
+        index: true
     },
-    routeId: {
-        type: String, // Matching Route.routeId
-        required: true
+    poolId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pool',
+        required: true,
+        index: true
     },
     seatsBooked: {
         type: Number,
         required: true,
-        min: [1, 'Must book at least 1 seat']
+        min: [1, 'Must book at least 1 seat'],
+        max: [4, 'Cannot book more than 4 seats at once']
+    },
+    farePaid: {
+        type: Number,
+        required: true,
+        min: [0, 'Fare cannot be negative']
     },
     status: {
         type: String,
--- models/Pool.js
+++ models/Pool.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// This model is a silod copy of the Pool model from Module 1 to maintain DDD
 const poolSchema = new mongoose.Schema({
     routeId: {
         type: String,
@@ -21,9 +20,14 @@ const poolSchema = new mongoose.Schema({
         required: true,
         default: 3
     },
+    bookedSeats: {
+        type: Number,
+        required: true,
+        default: 0
+    },
     status: {
         type: String,
-        enum: ["waiting", "partially_full", "full", "assigned", "completed", "cancelled"],
+        enum: ["waiting", "partially_full", "full", "POOL_READY", "WAITING_FOR_PAYMENTS", "assigned", "completed", "cancelled"],
         default: "waiting",
         index: true
     },
@@ -32,11 +36,6 @@ const poolSchema = new mongoose.Schema({
         seats: { type: Number, required: true },
         joinedAt: { type: Date, default: Date.now }
     }],
-    driverId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Driver',
-        default: null
-    },
     createdAt: {
         type: Date,
         default: Date.now
